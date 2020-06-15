68067398c56d942f444df7113a433c12829d293e
Manjiri <manjirikolte26@gmail.com>
2020-06-15 22:29:08 +0530

    30 Days hackerRank Challenge

--- src/Arrays/Solution.java
+++ src/Arrays/Solution.java
@@ -1,5 +1,40 @@
 package Arrays;
 
+/*
+ * Sample Input
+ * 4
+ * 1 4 3 2
+ * 
+ * Sample Output
+ * 2 3 4 1
+ * 
+ */
+
+import java.util.*;
+
 public class Solution {
 
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+            
+        }
+        for(int j=arr.length-1;j>=0;j--){
+            System.out.print(arr[j]+ " ");
+        }
+        scanner.close();
+    }
 }
+
--- src/Dictonaries_andMaps/Solution.java
+++ src/Dictonaries_andMaps/Solution.java
@@ -1,5 +1,46 @@
 package Dictonaries_andMaps;
 
-public class Solution {
+/*
+ * Sample Input
+ * 3
+ * sam 99912222
+ * tom 11122222
+ * harry 12299933
+ * sam
+ * edward
+ * harry
+ * 
+ * Sample Output
+ * sam=99912222
+ * Not found
+ * harry=12299933
+ * 
+ */
 
+//Complete this code or write your own from scratch
+import java.util.*;
+
+class Solution{
+  public static void main(String []argh){
+      Scanner in = new Scanner(System.in);
+    HashMap<String,Integer> myMap = new HashMap<String,Integer>();
+      int n = in.nextInt();
+      for(int i = 0; i < n; i++){
+          String name = in.next();
+          int phone = in.nextInt();
+          myMap.put(name,phone);
+
+      }
+      while(in.hasNext()){
+          String s = in.next();
+          // Write code here
+          if(myMap.get(s)==null){
+                System.out.println("Not found"); 
+          }else{
+              System.out.println(s+"="+myMap.get(s)); 
+          }
+             
+        }
+      in.close();
+  }
 }
